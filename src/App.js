8bcfcafac79bc08a369c8b9cf5b9fb7282247222
neoprint3d <drew@ronsman.com>
2021-10-30 10:26:18 -0500

    new

--- src/App.js
+++ src/App.js
@@ -4,17 +4,17 @@ import firebaseConfig from "./config";
 import firebase from "firebase/compat";
 import { getAnalytics } from "firebase/analytics";
 
-const app=firebase.initializeApp(firebaseConfig);
-const anylitics=getAnalytics(app);
-
+const app = firebase.initializeApp(firebaseConfig);
+const anylitics = getAnalytics(app);
 
 function App() {
   let dt = new Date();
-  const [time,setTime] = useState(0);
-  let [period,setPeriod] = useState(0);
+  const [time, setTime] = useState(0);
+  let [period, setPeriod] = useState(0);
+  let [size, setSize] = useState(0);
 
+  const top = "flex justify-center p-2";
 
- 
   useEffect(() => {
     let interval = setInterval(() => {
       setTime(new Date().toLocaleTimeString());
@@ -23,129 +23,100 @@ function App() {
   }, []);
 
   useEffect(() => {
-    console.log(time)
-    checktime()
+    checktime();
   }, [time]);
 
-  const checktime=()=>{
-    let t=new Date();
-    let minutes=t.getMinutes();
-    if(dt.getMinutes()<10){
-      minutes="0"+minutes;
+  const checktime = () => {
+    let t = new Date();
+    let minutes = t.getMinutes();
+    if (dt.getMinutes() < 10) {
+      minutes = "0" + minutes;
     }
 
+    let total = t.getHours() + "" + minutes;
+    if (total >= 720 && total <= 806) {
+      setPeriod(1);
+    } else if (total >= 806 && total <= 857) {
+      setPeriod(2);
+    } else if (total >= 902 && total <= 951) {
+      setPeriod(3);
+    } else if (total >= 956 && total <= 1042) {
+      setPeriod(4);
+    } else if (total >= 1047 && total <= 1207) {
+      setPeriod(5);
+    } else if (total >= 1212 && total <= 1258) {
+      setPeriod(6);
+    } else if (total >= 1303 && total <= 1349) {
+      setPeriod(7);
+    } else if (total >= 1354 && total <= 1440) {
+      setPeriod(8);
+    } else {
+      setPeriod("off time");
+    }
+  };
 
-    let total=(t.getHours()+""+minutes)
-    console.log(total)
-    if(total>=720 && total<=806){
-      setPeriod(1)
-    }
-    else if(total>=806 && total<=857){
-      setPeriod(2)
-    }
-    else if(total>=902 && total<=951){
-      setPeriod(3)
-    }
-    else if(total>=956 && total<=1042){
-      setPeriod(4)
-    }
-    else if(total>=1047 && total<=1207){
-      setPeriod(5)
-    }
-    else if(total>=1212 && total<=1258){
-      setPeriod(6)
-    }
-    else if(total>=1303 && total<=1349){
-      setPeriod(7)
-    }
-    else if(total>=1354 && total<=1440){
-      setPeriod(8)
-    }
-    else if(total>=1445){
-      setPeriod("school is done")
-    }
-   
-    
-
-
-    
-    
-  }
-        
+const makeBigger = () => {
+  setSize(size + 10);
+  console.log(size);
+  console.log(`p-${size}`);
+};
 
 
 
 
-   
+
+
 
   return (
-
-    
-    <div className="bg-green-200 h-screen">
-
-
-
-
-
-
-      <header className="bg-yellow-400 p-3 text-center rounded-xl mb-6">
-     
+    <main className="bg-green-200 h-screen">
+      <header className="bg-yellow-400 p-3 text-center rounded-b-3xl mb-6">
         <h1 className="text-3xl font-bold bg-yellow-200 p-3 rounded-2xl mb-3">
           Timer App
         </h1>
         <div className="flex text-center justify-center ">
-          <h5 className="flex-initial bg-yellow-200 rounded-2xl p-2.5 font-bold" >made by Drew</h5>
+          <button onclick={()=>makeBigger} className= {`flex-initial bg-yellow-200 rounded-2xl p-${size} font-bold hover:bg-red-800`}>
+            made by Drew
+          </button>
         </div>
       </header>
-      <div className="items-center">
-        <div className="bg-blue-800 text-center p-3 rounded-3xl mb-5">
-          <div className="flex justify-center p-2">
-            <h1 className=" flex-initial text-3xl text-bold rounded-3xl px-5 p-3 bg-purple-300">
-              Date
-            </h1>
+      <body>
+        <div className="flex justify-center">
+          <div className="bg-blue-800 text-center p-3 x-5 rounded-3xl mb-5 w-64 ">
+            <div className=" justify-center p-2">
+              <h1 className="text-3xl text-bold rounded-3xl px-5 p-3 bg-purple-300 font-bold">
+                Date
+              </h1>
+            </div>
+            <h3 className="text-2xl text-bold bg-indigo-200 rounded-3xl p-2 font-light ">
+              {dt.getMonth()}/{dt.getDate()}/{dt.getFullYear()}
+            </h3>
           </div>
-          <h3 className="text-2xl text-bold bg-indigo-200 rounded-3xl p-2" >
-            {dt.getMonth()}/{dt.getDate()}/{dt.getFullYear()}
-          </h3>
         </div>
-
-
-        <div className="bg-red-800 text-center p-3 rounded-3xl mb-5" >
+        <div className="bg-red-800 text-center p-3 rounded-3xl mb-5 container mx-auto">
           <div className="flex justify-center p-2">
-            <h1 className=" flex-initial text-3xl text-bold rounded-3xl px-5 p-3 bg-red-300" >
+            <h1 className=" text-3xl text-bold rounded-3xl px-5 p-3 bg-red-300 font font-bold">
               Time
             </h1>
           </div>
-          <h3 className="text-2xl text-bold bg-red-200 rounded-3xl p-2" >
-            {time}   
+          <h3 className="text-2xl text-bold bg-red-200 rounded-3xl p-2 font-light">
+            {time}
           </h3>
         </div>
 
-
-
-        <div className="bg-green-800 text-center p-3 rounded-3xl">
+        <div className="bg-green-800 text-center p-3 rounded-3xl bg-transparent">
           <div className="flex justify-center p-2">
-            <h1 className=" flex-initial text-3xl text-bold rounded-3xl px-5 p-3 bg-green-300">
+            <h1 className="  text-3xl text-bold rounded-3xl px-5 p-3 bg-green-300 font-bold">
               Period
             </h1>
           </div>
-          <h3 className="text-2xl text-bold bg-green-200 rounded-3xl p-2">
+          <h3 className="text-2xl text-bold  bg-green-200 rounded-3xl p-2 font-light">
             {period}
           </h3>
         </div>
-      </div>
-    </div>
-
-
-
+      </body>
+    </main>
   );
 }
-
-
-
-
-
-
 
 
 
