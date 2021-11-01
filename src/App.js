5b1be3483203266e3bd2d8ed0d5c6e0917ef17ec
neoprint3d <drew@ronsman.com>
2021-11-01 15:53:46 -0500

    functionalized components

--- src/App.js
+++ src/App.js
@@ -8,16 +8,16 @@ const app = firebase.initializeApp(firebaseConfig);
 getAnalytics(app);
 
 function App() {
-  let dt = new Date();
+  let dt = new Date(); //example time 'December 17, 1995 09:52:00'
   const [time, setTime] = useState(0);
   let [period, setPeriod] = useState(0);
   let [timeleft, setTimeleft] = useState(0);
-  const endof=[806,857,951,1042,1207,1258,1349,1440]
+  const endof = [806, 857, 951, 1042, 1207, 1258, 1349, 1440];
   let date = `${dt.getMonth() + 1}/${dt.getDay()}/${dt.getFullYear()}`;
 
   useEffect(() => {
     let interval = setInterval(() => {
-      setTime(new Date().toLocaleTimeString());
+      setTime(dt.toLocaleTimeString());
     }, 1000);
     return () => clearInterval(interval);
   }, []);
@@ -27,104 +27,114 @@ function App() {
   }, [time]);
 
   const checktime = () => {
-    let t = new Date();
-    let minutes = t.getMinutes();
+
+    console.log('hi')
+    let minutes = dt.getMinutes();
     if (dt.getMinutes() < 10) {
       minutes = "0" + minutes;
     }
-    
-    let total = t.getHours() + "" + minutes;
-
-
-    
-    if (total >= 720 && total <= 806) {
-      setPeriod(1);
-    } else if (total >= 806 && total <= 857) {
-      setPeriod(2);
-    } else if (total >= 902 && total <= 951) {
-      setPeriod(3);
-    } else if (total >= 956 && total <= 1042) {
-      setPeriod(4);
-    } else if (total >= 1047 && total <= 1207) {
-      setPeriod(5);
-    } else if (total >= 1212 && total <= 1258) {
-      setPeriod(6);
-    } else if (total >= 1303 && total <= 1349) {
-      setPeriod(7);
-    } else if (total >= 1354 && total <= 1440) {
-      setPeriod(8);
+    //gets period
+    let total = dt.getHours() + "" + minutes;
+    console.log(total);
+    if (total >= 720 && total <= 1440) {
+      if (total >= 720 && total <= 806) {
+        setPeriod(1);
+      }
+      if (total >= 806 && total <= 857) {
+        setPeriod(2);
+      }
+      if (total >= 902 && total <= 951) {
+        setPeriod(3);
+      }
+      if (total >= 956 && total <= 1042) {
+        setPeriod(4);
+      }
+      if (total >= 1047 && total <= 1207) {
+        setPeriod(5);
+      }
+      if (total >= 1212 && total <= 1258) {
+        setPeriod(6);
+      }
+      if (total >= 1303 && total <= 1349) {
+        setPeriod(7);
+      }
+      if (total >= 1354 && total <= 1440) {
+        setPeriod(8);
+      } else {
+        setPeriod("In Between bells");
+      }
     } else {
-      setPeriod("off time");
+      setPeriod("Out of School");
     }
-
-
-
-
-
-    if(period==0){
-      setTimeleft(endof[0]-total);
-    }
-    else{
-      setTimeleft(endof[period-1]-total);
+      //time left
+    if (total >= 720 && total <= 1440) {
+      if (period == 0) {
+        setTimeleft(`${endof[0] - total} min`);
+      } else if (period !=='In Between bells') {
+        setTimeleft(`${endof[period- 1] - total}`);
+      }
+      else {
+        setTimeleft("You're here in time");
+      }
+    } else {
+      setTimeleft("School is done");
     }
   };
 
   return (
-    <main className="h-screen bg-green-200">
+    <main className={`h-screen bg-blue-200`}>
       <header className="bg-yellow-600 p-3 text-center rounded-b-3xl mb-6 justify-center">
         <div className="bg-yellow-400 p-1.5 text-center rounded-3xl justify-center">
-        <div className="flex justify-center p-2">
-          <h1 className=" text-3xl font-bold rounded-3xl px-10 p-3 bg-yellow-200  ">
-            MISD Time
-          </h1>
-        </div>
-      
-        <div className="flex justify-center p-2">
-          <h1 className="rounded-3xl px-5 p-2 bg-yellow-200 font font-light">
-            Made By Drew Ronsman
-          </h1>
-        </div>
+          <div className="flex justify-center p-2">
+            <button className=" text-3xl font-bold rounded-3xl px-10 p-3 bg-yellow-200" onClick={checktime}>
+              MISD Time
+            </button>
+          </div>
+          <div className="flex justify-center p-2">
+            <h1 className="rounded-3xl px-5 p-2 bg-yellow-200 font font-light">
+              Made By Drew Ronsman
+            </h1>
+          </div>
         </div>
       </header>
       <body className="bg-green-200 h-screen">
-
-        <div className="bg-blue-800 text-center p-3 rounded-3xl mb-5 container mx-auto">
-          <div className="flex justify-center p-2">
-            <h1 className=" text-3xl text-bold rounded-3xl px-5 p-3 bg-blue-300 font font-bold">
-              Date
-            </h1>
-          </div>
-          <h3 className="text-2xl text-bold bg-blue-200 rounded-3xl p-2 font-light">
-            {date}
-          </h3>
+        <div className="justify-evenly flex">
+          <InfoCard color="blue" title="Date" text={date} />
+          <InfoCard color="red" title="Time" text={time} />
         </div>
-
-        <div className="bg-red-800 text-center p-3 rounded-3xl mb-5 container mx-auto">
-          <div className="flex justify-center p-2">
-            <h1 className=" text-3xl text-bold rounded-3xl px-5 p-3 bg-red-300 font font-bold">
-              Time
-            </h1>
-          </div>
-          <h3 className="text-2xl text-bold bg-red-200 rounded-3xl p-2 font-light">
-            {time}
-          </h3>
-        </div>
-
-        <div className="bg-green-800 text-center p-3 rounded-3xl mb-5 container mx-auto">
-          <div className="flex justify-center p-2">
-            <h1 className=" text-3xl text-bold rounded-3xl px-5 p-3 bg-green-300 font font-bold">
-              Period
-            </h1>
-          </div>
-          <h3 className="text-2xl text-bold bg-green-200 rounded-3xl p-2 font-light">
-            {period} 
-          </h3>
+        <div className="justify-evenly flex">
+          <InfoCard color="green" title="Period" text={period} />
+          <InfoCard color="pink" title="Time Left" text={timeleft} />
         </div>
       </body>
     </main>
   );
 }
 
-
+function InfoCard(props) {
+  const {color, title, text} = props;
+  return (
+    <div className={`p-3 lg:p-5 bg-${color}-800 rounded-3xl mb-5`}>
+      <div
+        className={`bg-${color}-600 text-center p-3 lg:p-5 rounded-2xl  mx-auto`}
+      >
+        <div className="flex justify-center p-2">
+          <h1
+            className={`text-2xl text-bold rounded-2xl  px-10 md:px-10 p-2 bg-${color}-300 font font-bold`}
+          >
+            {title}
+          </h1>
+        </div>
+        <div className="flex justify-center">
+          <h3
+            className={`text-xl text-bold bg-${color}-200 rounded-3xl lg:px-32 p-3 font-light`}
+          >
+            {text}
+          </h3>
+        </div>
+      </div>
+    </div>
+  );
+}
 
 export default App;
